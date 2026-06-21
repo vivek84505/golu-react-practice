f79cd11bffb27037c09d221cb07b5467627fdc50
Vivek <vivk84505@gmail.com>
2026-06-21 09:19:07 +0530

    user form details

--- src/component/UserForm.js
+++ src/component/UserForm.js
@@ -9,13 +9,15 @@ function UserForm() {
     // Form handling + Validation
     
   const [formData,setFormData] = useState({
-    name:"vivek D",
-    email:"vivek@gmail.com",
-    mobile:"9403384505"
-  })  
-  
+    name:"",
+    email:"",
+    mobile:""
+  })   // State variable
 
- 
+  const [errors,setErros] = useState({}) // State variable
+  const [submittedData,setSubmittedData] = useState(null)  // State variable  
+  
+  
 
   const handleChange = (e) =>{ 
     
@@ -28,16 +30,80 @@ function UserForm() {
     )
   }
    
+  const validateForm = () =>{
+
+        let newErros = {}
+        console.log("newErros before=======>",newErros)
+        // To decide if an array or object is emtpy or not we use lenght. if length is 0 then array/object is empty. 
+
+
+        // validatiing form elements using formdaData
+        if(!formData.name.trim()){
+            newErros.name = "Name is required"
+        }
+        
+        // here we are checking if value is blank or not + we need to check if the value is valid Email or not
+        let emailRegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+        
+        if(!formData.email.trim()){
+            newErros.email = "Email is required"
+        }
+        else if(!emailRegExp.test(formData.email)){
+             newErros.email = "Invalid Email"
+        }
+
+
+         // here we are checking if value is blank or not + we need to check if the value is valid Mobile or not
+        //let mobileRegExp = ^[789]\d{9}$
+
+        let mobileRegExp = /^(\+91|\+91\-|0)?[789]\d{9}$/
+        if(!formData.mobile.trim()){
+            newErros.mobile = "Mobile is required"
+        }
+        else if(!mobileRegExp.test(formData.mobile)){
+            newErros.mobile = "Invalid Mobile"
+        }
+
+        console.log("newErros after=======>",newErros)
+
+        setErros(newErros) // Adding error data into state errors object
  
 
-  console.log("formData========>",formData)
+        return Object.keys(newErros).length === 0
+         
+
+  }
+
+
+  const handleSubmit = (e) => {
+    
+     e.preventDefault();
+     
+     if(validateForm()){ 
+        setSubmittedData(formData)
+        console.log("Form Validated Submitting data to API=======>")
+     }
+     else{
+        setSubmittedData(null)
+     }
+    
+
+     
+   
+
+    // Goint to call API once the form is validated
+         //console.log("Form Validated Submitting data to API=======>")
+  }
+ 
+
+//   console.log("formData========>",formData)
  
   return (
     <div className="container mt-4">
       <h2>User Form</h2>
  
 
-      <form>
+      <form method="POST"  onSubmit={handleSubmit}>
         <div className="row">
           <div className="col-md-4">
             <div className="form-group mb-3">
@@ -51,6 +117,7 @@ function UserForm() {
                 value={formData.name}
                 onChange={handleChange}
               />
+              <small className="text-danger">{errors.name}</small>
             </div>
           </div>
 
@@ -66,6 +133,7 @@ function UserForm() {
                 value={formData.email}
                 onChange={handleChange}
               />
+               <small className="text-danger">{errors.email}</small>
             </div>
           </div>
 
@@ -81,6 +149,7 @@ function UserForm() {
                 value={formData.mobile}
                 onChange={handleChange}
               />
+               <small className="text-danger">{errors.mobile}</small>
             </div>
           </div>
         </div>
@@ -88,7 +157,26 @@ function UserForm() {
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+
       </form>
+
+        {submittedData && (
+             <div className="card mt-4 p-3">
+                <h4>Submitted Data</h4>
+                <p>
+                    <strong>Name:</strong> {submittedData.name}
+                </p>
+                <p>
+                    <strong>Email:</strong> {submittedData.email}
+                </p>
+
+                <p>
+                    <strong>Mobile:</strong> {submittedData.mobile}
+                </p>
+            </div>
+        )}
+
+
     </div>
   );
 }
